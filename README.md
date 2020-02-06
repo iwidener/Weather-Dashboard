424db9daeaf67c294b0dbaa31ad437f58ac93b22
Inna Widener <iwide2019@gmail.com>
2020-02-05 22:06:35 -0600

    5-Day Forecast Working

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 As a traveler 
 I want to see the weather outlook for multiple cities
 so that I can plan a trip accordingly.
+
+The weather data for cities is retrieved from OpenWeather API. The app runs in the browser. 
+Enter any city name in the search box and it will display the following under current weather conditions: city, date, icon image (visual representation of weather conditions), temperature, humidity, wind speed, and UV index. 
+
+A 5-Day Forecast below the current weather conditions are included. Each day for the 5-Day Forecast displays the following: date, icon image (visual representation of weather conditions), temperature, humidity.
+
+A search history allows the users to access their past search terms. Clicking on the city name will perform a new search that returns current and future conditions for that city. 
--- script.js
+++ script.js
@@ -25,8 +25,8 @@ $(document).ready(function () {
                 var forecast = "";
 
                 forecast += "<p><b>" + data.name + " (" + new Date().toDateString() + ")</b><img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\"></p>" +
-                    data.main.temp + "&deg;F" + " | " + data.weather[0].main + ", " + data.weather[0].description + " | humidity: " + data.main.humidity + "%"
-            
+                    " Temperature: " + data.main.temp + "&deg;F" + " | " + " Wind speed: " + data.wind.speed + " | Humidity: " + data.main.humidity + "%"
+
                 $.ajax({
                     url: "http://api.openweathermap.org/data/2.5/uvi",
                     dataType: "json",
@@ -39,11 +39,46 @@ $(document).ready(function () {
 
                         $("#current-section").html(forecast);
                     }
-
                 })
 
-            }
+                //API call for 5-day forecast.
 
+                $.ajax({
+                    url: "http://api.openweathermap.org/data/2.5/forecast",
+                    dataType: "json",
+                    method: "GET",
+                    data: { q: city, appid: apiKey, units: "imperial" },
+
+                    success: function (data) {
+                        console.log(data);
+                        var forecastFive = "";
+
+                        forecastFive += "<p><b>" + data.list[0].dt_txt + "</b><img src=\"http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png\"></p>" +
+                            " Temperature: " + data.list[0].main.temp + "&deg;F" + " | " + " Humidity: " + data.list[0].main.humidity + "%"
+
+                        forecastFive += "<p><b>" + data.list[8].dt_txt + "</b><img src=\"http://openweathermap.org/img/w/" + data.list[7].weather[0].icon + ".png\"></p>" +
+                            " Temperature: " + data.list[8].main.temp + "&deg;F" + " | " + " Humidity: " + data.list[8].main.humidity + "%"
+
+                        forecastFive += "<p><b>" + data.list[16].dt_txt + "</b><img src=\"http://openweathermap.org/img/w/" + data.list[16].weather[0].icon + ".png\"></p>" +
+                            " Temperature: " + data.list[16].main.temp + "&deg;F" + " | " + " Humidity: " + data.list[16].main.humidity + "%"
+
+                        forecastFive += "<p><b>" + data.list[24].dt_txt + "</b><img src=\"http://openweathermap.org/img/w/" + data.list[24].weather[0].icon + ".png\"></p>" +
+                            " Temperature: " + data.list[24].main.temp + "&deg;F" + " | " + " Humidity: " + data.list[24].main.humidity + "%"
+
+                        forecastFive += "<p><b>" + data.list[32].dt_txt + "</b><img src=\"http://openweathermap.org/img/w/" + data.list[32].weather[0].icon + ".png\"></p>" +
+                            " Temperature: " + data.list[32].main.temp + "&deg;F" + " | " + " Humidity: " + data.list[32].main.humidity + "%"
+
+
+                        //Results are printed in 5-Day Forecast.
+                        $("#future-section").html(forecastFive);
+
+                        //City name is printed in search form.
+                        $(".list-group").html(city);
+
+                    }
+                    //Local storage
+                })
+            }
         });
     });
 })
